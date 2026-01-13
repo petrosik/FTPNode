c4d98b2da96bceb9f9cccd816da3c42a6dbd7462
petrosik <petrosik.301@gmail.com>
2026-01-13 10:48:06 +0100

    stuff

---
+++ WebFTPViewer/Controllers/FtpSetupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace WebFTPViewer.Controllers
+{
+    [Route("api/ftp")]
+    [ApiController]
+    public class FtpSetupController : ControllerBase
+    {
+        [HttpPost("Login")]
+        public IActionResult Login([FromBody] LoginJson info)
+        {
+            
+            return Ok(new { Message = "FTP Setup Endpoint is working." });
+        }
+    }
+}
---
+++ WebFTPViewer/Program.cs
@@ -0,0 +1,47 @@
+using WebFTPViewer.Client.Pages;
+using WebFTPViewer.Components;
+
+namespace WebFTPViewer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
